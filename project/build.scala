b74a4ced01da527f0f892aa8763bbc94e0e30d9b
Vincent Spiewak <vspiewak@gmail.com>
2013-07-19 21:19:48 +0200

    first commit

---
+++ project/build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+
+object JavaSampleBuild extends Build {
+  def scalaSettings = Seq(
+    scalaVersion := "2.10.0",
+    scalacOptions ++= Seq(
+      "-optimize",
+      "-unchecked",
+      "-deprecation"
+    )
+  )
+
+  def buildSettings =
+    Project.defaultSettings ++
+    scalaSettings
+
+  lazy val root = {
+    val settings = buildSettings ++ Seq(name := "opencv-scala-sample")
+    Project(id = "opencv-scala-sample", base = file("."), settings = settings)
+  }
+}
+
