f157be44fe4420e770bdb59bbe650c75e4bfcd98
Vincent Spiewak <vspiewak@gmail.com>
2013-07-20 02:26:27 +0200

    Fix Indentation

--- src/main/java/WebcamDemo.java
+++ src/main/java/WebcamDemo.java
@@ -7,33 +7,33 @@ import org.opencv.imgproc.Imgproc;
 
 public class WebcamDemo {
 
-	public void run() {
-		
-		VideoCapture cap = new VideoCapture(0);
-		
-		if(!cap.isOpened()) {
-			System.out.println("Webcam not found :(");
-		} else {
-			System.out.println("Found Webcam: " + cap);
-      try {
-			System.out.println("Wait a bit, Webcam start");
-      Thread.sleep(2000);
-      } catch(InterruptedException ie) {
-      }
-		}
-		
-		Mat frame = new Mat();
-		cap.read(frame);
-		Highgui.imwrite("webcam.jpg", frame);
+    public void run() {
 
-		Mat frameBlur = new Mat();
-		Imgproc.blur(frame, frameBlur, new Size(10, 10));
-		Highgui.imwrite("webcam-blurred.jpg", frameBlur);
-		
-		Imgproc.GaussianBlur(frame, frameBlur, new Size(25, 25), 30);
-		Highgui.imwrite("webcam-gaussian.jpg", frameBlur);
-		
-		cap.release();
-		
-	}
+        VideoCapture cap = new VideoCapture(0);
+
+        if (!cap.isOpened()) {
+            System.out.println("Webcam not found :(");
+        } else {
+            System.out.println("Found Webcam: " + cap);
+            try {
+                System.out.println("Wait a bit, Webcam start");
+                Thread.sleep(2000);
+            } catch (InterruptedException ie) {
+            }
+        }
+
+        Mat frame = new Mat();
+        cap.read(frame);
+        Highgui.imwrite("webcam.jpg", frame);
+
+        Mat frameBlur = new Mat();
+        Imgproc.blur(frame, frameBlur, new Size(10, 10));
+        Highgui.imwrite("webcam-blurred.jpg", frameBlur);
+
+        Imgproc.GaussianBlur(frame, frameBlur, new Size(25, 25), 30);
+        Highgui.imwrite("webcam-gaussian.jpg", frameBlur);
+
+        cap.release();
+
+    }
 }
